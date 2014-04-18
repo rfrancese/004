2257ed18adcd77f4bf981f7e96df103b68bf9ced
carmine scala <carminuccio2792@hotmail.it>
2014-04-18 19:06:34 +0200

    Pag Cerca

--- Buonotourist-Unisa/gen/com/example/proveandroid/R.java
+++ Buonotourist-Unisa/gen/com/example/proveandroid/R.java
@@ -991,6 +991,7 @@ containing a value of this type.
         public static final int action_mode_close_button=0x7f050024;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
+        public static final int arrivo=0x7f05003f;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
@@ -1005,7 +1006,7 @@ containing a value of this type.
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
-        public static final int idBottoni_cerca=0x7f05003c;
+        public static final int idBottoni_cerca=0x7f05003d;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
@@ -1014,9 +1015,11 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int partenza=0x7f05003e;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int scroll=0x7f05003c;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1089,10 +1092,14 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int cerca=0x7f030017;
+        public static final int main=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class string {
+        public static final int A=0x7f0a0015;
+        public static final int Da=0x7f0a0014;
+        public static final int DaCheOra=0x7f0a0016;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
         public static final int abc_action_bar_home_description=0x7f0a0001;
@@ -1133,7 +1140,9 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
+        public static final int annulla=0x7f0a0013;
         public static final int app_name=0x7f0a000d;
+        public static final int cerca=0x7f0a0012;
         public static final int tab1=0x7f0a000e;
         public static final int tab2=0x7f0a000f;
         public static final int tab3=0x7f0a0010;
