812c2d4a0f4a351f710ef7c0aec5f12af9c58c4b
Francesco Genio <francesco.genio@gmail.com>
2014-04-25 01:06:53 +0200

    PERFEZIONATO STILE

--- Buonotourist-Unisa/src/com/example/proveandroid/CercaActivity.java
+++ Buonotourist-Unisa/src/com/example/proveandroid/CercaActivity.java
@@ -3,7 +3,6 @@ package com.example.proveandroid;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
@@ -12,7 +11,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class CercaActivity extends Activity  {
