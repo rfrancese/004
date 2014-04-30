a314a371fb7eac28514fd917e06a864f5eab09e6
Francesco Genio <francesco.genio@gmail.com>
2014-04-30 16:07:26 +0200

    FATTO LAYOUT CORSE

--- Buonotourist-Unisa/gen/com/example/proveandroid/R.java
+++ Buonotourist-Unisa/gen/com/example/proveandroid/R.java
@@ -980,10 +980,11 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int border_navbar_sfondo=0x7f020057;
-        public static final int bottoni_body_sfondo=0x7f020058;
-        public static final int bottoni_select_body_sfondo=0x7f020059;
-        public static final int icon_buonotourist=0x7f02005a;
-        public static final int reverse_icon=0x7f02005b;
+        public static final int bottone_navbar_reverse=0x7f020058;
+        public static final int bottoni_body_sfondo=0x7f020059;
+        public static final int bottoni_select_body_sfondo=0x7f02005a;
+        public static final int icon_buonotourist=0x7f02005b;
+        public static final int reverse_icon=0x7f02005c;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
