129075b20492fdb9aaa683f69938d14b6902635b
carmine scala <carminuccio2792@hotmail.it>
2014-05-04 16:10:17 +0200

    inserimento paesi nella finestra di dialogo relativa a pag tariffe, andata e ritorno

--- Buonotourist-Unisa/src/com/activity/principali/TariffeActivity.java
+++ Buonotourist-Unisa/src/com/activity/principali/TariffeActivity.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,10 +17,13 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -31,7 +35,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class TariffeActivity extends Activity {
-
+    
+	private MioDbHelper mMioDbHelper = null;
 	private static final int TariffaDa = 1;
 	private static final int TariffaA = 2;
 	private static final int Alert = 3;
@@ -42,7 +47,10 @@ public class TariffeActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tariffe);
 		
-		
+		mMioDbHelper = new MioDbHelper(getApplicationContext());
+		 delete();
+		 riempiDB();
+		 
 		View widgetTariffaDa=findViewById(R.id.idBottoni_TariffaDa);
 		View widgetTariffaA=findViewById(R.id.idBottoni_TariffaA);
 		
@@ -165,7 +173,16 @@ public class TariffeActivity extends Activity {
 	
 public Dialog TariffaDa(){
 		
-		final String[] options = { "Nola", "Sarno", "Caserta", "Palma Campania", "San Paolo Bel Sito" };
+	
+	    ArrayList<String> options1=tuttoTranneFiscianoLancusi();
+		
+		final String options[]=new String[options1.size()];
+		int lunghezza=options1.size();
+		
+		for(int i=0;i<lunghezza;i++){
+			options[i]=options1.get(i);
+		}
+		
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle(getString(R.string.TariffaDa));
 		builder.setSingleChoiceItems(options, 139, new DialogInterface.OnClickListener() {
@@ -209,7 +226,8 @@ public Dialog TariffaDa(){
 	
 public Dialog TariffaA(){
 		
-		final String[] options = {"Fisciano","Lancusi"};
+		final String[] options;
+		options=soloUniversita();
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle(getString(R.string.TariffaA));
 		builder.setSingleChoiceItems(options, 2, new DialogInterface.OnClickListener() {
@@ -399,5 +417,91 @@ public Dialog Alert(){
     	super.onConfigurationChanged(newConfig);     
     }
     
-	
+    public void riempiDB(){
+    	SQLiteDatabase db = mMioDbHelper.getWritableDatabase();
+    	ContentValues values= new ContentValues();
+    	
+    	values.put("nome", "Fisciano");
+    	long id1=db.insert("rubrica", null, values);
+    	values.put("nome", "Lancusi");
+    	long id2=db.insert("rubrica", null, values);
+    	values.put("nome", "Cimitile");
+    	long id22=db.insert("rubrica", null, values);
+    	values.put("nome", "San Paolo Bel Sito");
+    	long id23=db.insert("rubrica", null, values);
+    	values.put("nome", "Sicignano Scalo");
+    	long id31=db.insert("rubrica", null, values);
+    	values.put("nome", "Perrazze");
+    	long id21=db.insert("rubrica", null, values);
+    	values.put("nome", "Contursi");
+    	long id112=db.insert("rubrica", null, values);
+    	values.put("nome", "Eboli");
+    	long id3=db.insert("rubrica", null, values);
+    	values.put("nome", "Caserta");
+    	long id4=db.insert("rubrica", null, values);
+    	values.put("nome", "Castel Cisterna");
+    	long id5=db.insert("rubrica", null, values);
+    	values.put("nome", "S.Gennaro Vesuviano");
+    	long id6=db.insert("rubrica", null, values);
+    	values.put("nome", "Sarno");
+    	long id7=db.insert("rubrica", null, values);
+    	values.put("nome", "Pomigliano");
+    	long id8=db.insert("rubrica", null, values);
+    	values.put("nome", "Nola");
+    	long id9=db.insert("rubrica", null, values);
+    	values.put("nome", "Lauro");
+    	long id10=db.insert("rubrica", null, values);
+    	values.put("nome", "Boscoreale");
+    	long id11=db.insert("rubrica", null, values);
+    	values.put("nome", "Pompei");
+    	long id12=db.insert("rubrica", null, values);
+    	values.put("nome", "Nocera");
+    	long id13=db.insert("rubrica", null, values);
+    	values.put("nome", "Roccarainola");
+    	long id14=db.insert("rubrica", null, values);
+    	values.put("nome", "Cicciano");
+    	long id15=db.insert("rubrica", null, values);
+    	values.put("nome", "Palma Campania");
+    	long id16=db.insert("rubrica", null, values);
+    }
+    
+    
+    private String[] soloUniversita() {
+    	//Chiediamo l'accesso al db
+    	SQLiteDatabase db = mMioDbHelper.getReadableDatabase();
+        String[] universita=new String[2];
+    	
+    	final String sql = "SELECT * FROM rubrica where _id=1 or _id=2 order by nome";
+    	int i=0;
+    	Cursor c = db.rawQuery(sql, null);
+    	
+    	while(c.moveToNext()) { 
+    		universita[i]=c.getString(1);
+    		i++;
+    	}
+    	return universita;
+    }
+    
+    
+    private ArrayList<String> tuttoTranneFiscianoLancusi() {
+    	//Chiediamo l'accesso al db
+    	SQLiteDatabase db = mMioDbHelper.getReadableDatabase();
+    	ArrayList<String> paesi =new ArrayList<String>();
+    	
+    	final String sql = "SELECT * FROM rubrica where _id!=1 and _id!=2 order by nome";
+    	
+    	Cursor c = db.rawQuery(sql, null);
+    	
+    	while(c.moveToNext()) { 
+    		paesi.add(c.getString(1));
+    		//Toast.makeText(getApplicationContext(),""+c.getString(1), Toast.LENGTH_SHORT).show();
+    	}
+    	return paesi;
+    }
+    
+    
+    public void delete(){
+	   	SQLiteDatabase db = mMioDbHelper.getWritableDatabase();
+		int r=db.delete("rubrica", null, null);
+	}
 }
